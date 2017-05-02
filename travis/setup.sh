99065496be318dac4ad2a01869b03fff5d229d7f
AlCalzone <d.griesel@gmx.net>
2017-05-02 12:35:44 +0200

    Update setup.sh

--- travis/setup.sh
+++ travis/setup.sh
@@ -2,12 +2,12 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   # for linux builds, download and install the neccessary packages
-  sudo apt-get install python-software-properties
-  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  sudo apt-get install -y python-software-properties
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt-get update
-  sudo apt-get install gcc-4.8
+  sudo apt-get install -y gcc-4.8
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-  sudo apt-get install g++-4.8
+  sudo apt-get install -y g++-4.8
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 else
   # TODO: figure out what's neccessary for osx
