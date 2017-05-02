b350772670e8f7209f372039b8d886033985f40c
AlCalzone <d.griesel@gmx.net>
2017-05-02 12:23:35 +0200

    added setup script for travis tests

---
+++ travis/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  # for linux builds, download and install the neccessary packages
+  sudo apt-get install python-software-properties
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  sudo apt-get update
+  sudo apt-get install gcc-4.8
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+  sudo apt-get install g++-4.8
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  # TODO: figure out what's neccessary here
+fi
