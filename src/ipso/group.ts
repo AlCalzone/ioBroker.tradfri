e872fe78bf31cdc2305404445b49398975feae23
Dominic Griesel <d.griesel@gmx.net>
2017-09-11 21:11:19 +0200

    support renaming of devices
    fix error on state deletion, add channel for lightbulb
    
    support switching and renaming groups and selecting scenes

--- src/ipso/group.ts
+++ src/ipso/group.ts
@@ -14,6 +14,7 @@ export class Group extends IPSODevice {
 
 	@ipsoKey("9018")
 	@deserializeWith(obj => parseAccessoryLink(obj))
+	@serializeWith(ids => toAccessoryLink(ids))
 	public deviceIDs: number[];
 
 }
@@ -23,4 +24,11 @@ function parseAccessoryLink(link): number[] {
 	const hsLink = link["15002"];
 	const deviceIDs = hsLink["9003"];
 	return deviceIDs;
+}
+function toAccessoryLink(ids: number[]): any {
+	return {
+		15002: {
+			9003: ids,
+		},
+	};
 }
