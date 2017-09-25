b4fcaa731d64b5aef6121b1f3940e837135c9c14
dominic.griesel <d.griesel@gmx.net>
2017-09-25 13:16:55 +0200

    made deploy script more general

--- src/deploy_local.ts
+++ src/deploy_local.ts
@@ -1,37 +1,46 @@
-﻿/*
+﻿// tslint:disable:no-var-requires
+/*
 	Allows easier local debugging over SSH.
 	Running `npm run deploy_local` updates remote adapter files
 	and restarts the instance
 */
 
-import * as fs from "fs";
-import * as path from "path";
-import * as nodeSSH from "node-ssh";
-
-const ssh = new nodeSSH();
-
 /*
 	CONFIGURATION:
-		- enter adapter name behind ADAPTER_NAME
 		- provide a deploy_password.json file in the project root with contents
 			{
 				"host": "<HOSTNAME>",
 				"username": "<USERNAME>",
 				"password": "<PASSWORD>"
 			}
+		- specify which dirs and files should be uploaded
+		- specify where the root dir is relative to this script
 */
-const ADAPTER_NAME = "tradfri";
-const sshConfig = require("../deploy_password.json");
+const uploadDirs = ["admin", "build"];
+const uploadFiles = ["package.json", "io-package.json", "main.js"];
+const rootDir = "../";
 
+// =========================
+// CAN'T TOUCH THIS
+// =========================
 
-const localRoot = path.resolve(__dirname, "../");
+import * as fs from "fs";
+import * as nodeSSH from "node-ssh";
+import * as path from "path";
+
+const localRoot = path.resolve(__dirname, rootDir);
+
+const ioPack = require(path.join(rootDir, "io-package.json"));
+const ADAPTER_NAME = ioPack.common.name;
+
+const ssh = new nodeSSH();
+const sshConfig = require(path.join(rootDir, "deploy_password.json"));
+
 const remoteRoot = `/opt/iobroker/node_modules/iobroker.${ADAPTER_NAME}`;
-
 
 (async function main() {
 	await ssh.connect(sshConfig);
 
-	const uploadDirs = ["admin", "build"];
 	for (const dir of uploadDirs) {
 		console.log(`uploading ${dir} dir...`);
 		await ssh.putDirectory(path.join(localRoot, dir), path.join(remoteRoot, dir), {
@@ -40,10 +49,9 @@ const remoteRoot = `/opt/iobroker/node_modules/iobroker.${ADAPTER_NAME}`;
 			validate: (pathname) => {
 				const basename = path.basename(pathname);
 				return !basename.startsWith("deploy_");
-			}
+			},
 		});
 	}
-	const uploadFiles = ["package.json", "io-package.json", "main.js"];
 	for (const file of uploadFiles) {
 		console.log(`uploading ${file}...`);
 		await ssh.putFile(path.join(localRoot, file), path.join(remoteRoot, file));
