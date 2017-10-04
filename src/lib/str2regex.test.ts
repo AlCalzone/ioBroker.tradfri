390e730a7d07d022d891da1c04596913a8de9e56
dominic.griesel <d.griesel@gmx.net>
2017-10-04 15:52:45 +0200

    add tests for str2regex and fix bugs

---
+++ src/lib/str2regex.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { str2regex } from "./str2regex";
+// tslint:disable:no-unused-expression
+
+describe("lib/str2regex => ", () => {
+
+	it("should replace wildcards", () => {
+		expect(str2regex("*").toString()).to.equal("/.*/");
+	});
+	it("should replace dots", () => {
+		expect(str2regex(".").toString()).to.equal("/\./");
+	});
+	it("should replace combined patterns", () => {
+		expect(str2regex("*.*").toString()).to.equal("/.*\..*/");
+	});
+
+});
