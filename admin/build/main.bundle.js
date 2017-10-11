82a83f65c6d7de531ad33ee2764624b7531ff758
dominic.griesel <d.griesel@gmx.net>
2017-10-11 14:46:13 +0200

    setup react admin ui

---
+++ admin/build/main.bundle.js
@@ -0,0 +1,17 @@
+webpackJsonp(["main"],{
+
+/***/ "./admin/src/index.tsx":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__("./node_modules/react/index.js");
+const ReactDOM = __webpack_require__("./node_modules/react-dom/index.js");
+ReactDOM.render(React.createElement("h1", null, "Hello, World!"), document.getElementById("adapter-container"));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiRDovaW9Ccm9rZXIudHJhZGZyaS9hZG1pbi9zcmMvIiwic291cmNlcyI6WyJpbmRleC50c3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSwrQkFBK0I7QUFDL0Isc0NBQXNDO0FBRXRDLFFBQVEsQ0FBQyxNQUFNLENBQ2QsZ0RBQXNCLEVBQ3RCLFFBQVEsQ0FBQyxjQUFjLENBQUMsbUJBQW1CLENBQUMsQ0FDNUMsQ0FBQyJ9
+
+/***/ })
+
+},["./admin/src/index.tsx"]);
+//# sourceMappingURL=main.bundle.js.map
