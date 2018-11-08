9ed6bcc2d666ffa7c2f13c8b64c279451e3aab2b
dominic.griesel <d.griesel@gmx.net>
2018-11-08 15:09:02 +0100

    Strongly type adapter config

--- src/lib/ioBroker.d.ts
+++ src/lib/ioBroker.d.ts
@@ -1,4 +1,4 @@
-import fs = require("fs");
+import { native as AdapterConfig } from "../../io-package.json";
 
 // tslint:disable:no-namespace
 declare global {
@@ -921,7 +921,7 @@ declare global {
 			/** Namespace of adapter objects: "<name>.<instance>" */
 			readonly namespace: string;
 			/** native part of the adapter settings */
-			config: any;
+			config: typeof AdapterConfig;
 			/** common part of the adapter settings */
 			common: any;
 			/** system part of the adapter settings */
