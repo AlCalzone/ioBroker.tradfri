d173fac5d0c65f6ed592a41d11ed9d0032493ace
Dominic Griesel <d.griesel@gmx.net>
2021-05-04 13:20:30 +0200

    fix: escape backslash in str2regex

--- src/lib/str2regex.ts
+++ src/lib/str2regex.ts
@@ -1,6 +1,7 @@
 export function str2regex(pattern: string): RegExp {
 	return new RegExp(
 		pattern
+			.replace(/\\/g, "\\\\") // Punkte als solche matchen
 			.replace(/\./g, "\\.") // Punkte als solche matchen
 			.replace(/\*/g, ".*") // Wildcard in Regex umsetzen
 			.replace(/\!/g, "?!"), // negative lookahead
